40fd990b5735f8cba76ff205765191ba837acda1
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2020-12-06 05:54:05 -0500

    Initial commit

---
+++ DukeProject/src/lang.java
@@ -0,0 +1,10 @@
+
+public class lang {
+	public void runHello () {
+		FileResource res = new FileResource("hello_unicode.txt");
+		for (String line : res.lines()) {
+			System.out.println(line);
+		}
+	}
+
+}
